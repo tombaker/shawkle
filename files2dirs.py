993ec32d1dda7dffd972a567bd970a928b8fede7
Tom Baker <tom@tombaker.org>
2017-09-10 14:58:44 +0200

    delete extra files - shuffle.py (Python 3) is the one I use on daily basis

--- files2dirs.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('files2dirs.yaml') as yamlfile:
-    config = yaml.load(yamlfile)
-
-import pprint
-pprint.pprint(config['files2dirs'])
-
-print(config['files2dirs']['agendazz'])
